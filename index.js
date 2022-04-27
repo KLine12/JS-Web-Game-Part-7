482f59fbf5dd99c2775c5715ec9adf5b842dac88
KLine12 <bryanjass@gmail.com>
2022-04-26 20:21:22 -0500

    commiit with Bonus instructions

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ async function moveNPC(){
     await npc.walkNorth(400)
 }
 
+// Bonus
+// If you have time, below are some ideas to develop your mastery of async and await:
+
+// Loop the track, so once the character finishes, it starts to circle around the screen again.
+// You may have done this before in a previous lesson, but now that you are using async and await, try utilizing a while loop to implement this behavior.
+// Read about generator functions in MDN's documentation. The generator function is another block on which async and await are built. While it is uncommon to use the generator function syntax directly, it is a very interesting tool for customizing your functions.
+// Acceptance Criteria
+// When opening the game in the browser, the green character walks around the screen on the intended track and returns to its initial position.
+// When viewing newNonPlayableCharacter.js, function moveNPC() must exist.
+// When viewing newNonPlayableCharacter.js, function moveNPC() must be an async/await function that moves the character along the intended track.
+// Before submitting, make sure you do a self review of your code, check for formatting, spelling, include comments in your code, and ensure you have a healthy commit history.
+
+// Make sure to submit your github repository link on the submission page.
+
 
 
 
